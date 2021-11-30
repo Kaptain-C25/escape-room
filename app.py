3c371dadf7661081d789ca88d49e8b0cd552c317
Kaptain-C25 <95114372+Kaptain-C25@users.noreply.github.com>
2021-11-30 16:44:13 -0500

    First upload

---
+++ app.py
@@ -0,0 +1,30 @@
+import jinja2
+from cs50 import SQL
+from flask import Flask, redirect, render_template, request
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///inventory.db")
+
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+@app.route("/room_1", methods=["GET", "POST"])
+def room_1():
+    if request.method == "POST":
+        return render_template("room_1.html")
+    if request.method == "GET":
+        return render_template("room_1.html")
+
