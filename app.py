05a4fad3ff741bb921c9e7ed4645c5fd7e800e2c
Kevin <kevinchen1@college.harvard.edu>
2021-12-04 14:58:27 -0500

    December 4, 2021 2:58 PM

--- app.py
+++ app.py
@@ -11,7 +11,6 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 # Configure CS50 Library to use SQLite database
 db = SQL("sqlite:///inventory.db")
 
-location = 0
 solved = {0: True, 1: False, 2: False, 3: False, 4: False}
 
 def solve_required(loc):
@@ -34,18 +33,14 @@ def after_request(response):
 
 @app.route("/")
 def homepage():
-    global location
-    location = 0
     return render_template("homepage.html")
 
 @app.route("/room_1", methods=["GET", "POST"])
 @solve_required(1)
 def room_1():
-    global location
     global solved
     if request.method == "POST":
-        solved[location] = True
-        location += 1
+        solved[1] = True
         return redirect("/room_2", code=303)
     else:
         return render_template("room_1.html")
@@ -53,11 +48,9 @@ def room_1():
 @app.route("/room_2", methods=["GET", "POST"])
 @solve_required(2)
 def room_2():
-    global location
     global solved
     if request.method == "POST":
-        solved[location] = True
-        location += 1
+        solved[2] = True
         return redirect("/room_3", code=303)
     else:
         return render_template("room_2.html")
@@ -65,11 +58,9 @@ def room_2():
 @app.route("/room_3", methods=["GET", "POST"])
 @solve_required(3)
 def room_3():
-    global location
     global solved
     if request.method == "POST":
-        solved[location] = True
-        location += 1
+        solved[3] = True
         return redirect("/end")
     else:
         return render_template("room_3.html")
